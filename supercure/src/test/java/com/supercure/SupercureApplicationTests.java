bbe54e04c78aef237c0add5a6e401fe3c0cd4472
Rupali Chandrashekhar <Rupali.chandrashekar@TANDON.LOCAL>
2022-01-30 11:07:05 +0530

    initial commits

---
+++ supercure/src/test/java/com/supercure/SupercureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supercure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
